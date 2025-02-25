1ccc57793910ce4f81440e8146bc558484d0eb19
Valentina Gaggero <valentina.gaggero@iit.it>
2025-02-25 13:17:44 +0100

    EMS ver 200

--- emBODY/eBcode/arch-arm/board/ems004/appl/v2/cfg/eoemsappl/EOMtheEMSapplCfg_cfg.h
+++ emBODY/eBcode/arch-arm/board/ems004/appl/v2/cfg/eoemsappl/EOMtheEMSapplCfg_cfg.h
@@ -81,18 +81,34 @@ extern "C" {
 #define EOMTHEEMSAPPLCFG_VERSION_MAJOR          (VERSION_MAJOR_OFFSET+3)
 //  <o> minor           <0-255> 
 //  <o> minor           <0-255>
+<<<<<<< HEAD
 #define EOMTHEEMSAPPLCFG_VERSION_MINOR          220
+=======
+
+#define EOMTHEEMSAPPLCFG_VERSION_MINOR          200
+
+
+
+>>>>>>> 0c6e725d6 (EMS ver 200)
 //  </h>version
 
 //  <h> build date
 //  <o> year            <2010-2030>
 #define EOMTHEEMSAPPLCFG_BUILDDATE_YEAR         2025
 //  <o> month           <1-12>
+<<<<<<< HEAD
 #define EOMTHEEMSAPPLCFG_BUILDDATE_MONTH        5
 //  <o> day             <1-31>
 #define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          21
 //  <o> hour            <0-23>
 #define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         17
+=======
+#define EOMTHEEMSAPPLCFG_BUILDDATE_MONTH        2
+//  <o> day             <1-31>
+#define EOMTHEEMSAPPLCFG_BUILDDATE_DAY          18
+//  <o> hour            <0-23>
+#define EOMTHEEMSAPPLCFG_BUILDDATE_HOUR         14
+>>>>>>> 0c6e725d6 (EMS ver 200)
 //  <o> minute          <0-59>
 #define EOMTHEEMSAPPLCFG_BUILDDATE_MIN          17
 //  </h>build date
